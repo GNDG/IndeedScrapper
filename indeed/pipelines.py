0a251d018c67bbc08ad93918654d44373b602e60
vsnehil <vsnehil92@gmail.com>
2017-03-27 21:35:22 +0530

    comments added

--- indeed/pipelines.py
+++ indeed/pipelines.py
@@ -1,7 +1,4 @@
 # Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/topics/item-pipeline.html
 import sys
 import pymongo
 
@@ -13,18 +10,12 @@ class IndeedPipeline(object):
     def process_item(self, item, spider):
     
         i = item['summary'][0]
-        # i = remove_tags(i)
-        # i = replace_escape_chars(i)
         item['summary'][0] = i
 
         i = item['job_title'][0]
-        # i = remove_tags(i)
-        # i = replace_escape_chars(i)
         item['job_title'][0] = i
 
         i = item['company'][0]
-        # i = remove_tags(i)
-        # i = replace_escape_chars(i)
         item['company'][0] = i.strip()
         
         return item
@@ -46,7 +37,6 @@ class MongoDBPipeline(object):
                 raise DropItem("Missing {0}!".format(data))
         if valid:
             check=self.collection.find_one({"job_key" : item['job_key']})
-            #print(check)
             if not check:
                 self.collection.insert(dict(item))
                 log.msg("Question added to MongoDB database!",
